b942e2b651f56bd749a31099614d0edf4944838b
Shub <cptnshubham39@gmail.com>
2024-01-05 21:52:32 +0530

    Simple store and search

--- main.py
+++ main.py
@@ -1,56 +1,126 @@
 import os
 from pyfingerprint.pyfingerprint import PyFingerprint
 
+f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+
+def clear_database():
+    f.clearDatabase()
+    data=open('students.txt','w')
+    data.close()
+    print('')
+
 def enroll_fingerprint():
-    data_folder = 'fingerprint_data'
-
-    # Create the data folder if it doesn't exist
-    if not os.path.exists(data_folder):
-        os.makedirs(data_folder)
-
-    # Initialize the Fingerprint sensor
+    data=open('students.txt','a')
+    name=str(input("Enter Student Name: "))
+    #college_roll=str(input("Enter Student Roll no: "))
     try:
-        f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
-        if not f.verifyPassword():
-            raise ValueError('The fingerprint sensor password is incorrect')
+        if (f.verifyPassword() == False):
+            print('Contact Admin')
+            print('')
+            raise ValueError('The given fingerprint sensor password wrong')
+
     except Exception as e:
-        print('Error initializing the fingerprint sensor: ' + str(e))
-        return
+            print('Contact Admin')
+            print('')
+            print('The fingerprint sensor could not be initialized')
+            print('Exception message: ' + str(e))
+            print('') 
+            data.close()
+            return None
 
-    # Get the name of the fingerprint holder
-    name = input('Enter the name of the fingerprint holder: ')
+    print('Currently used templates: ' + str(f.getTemplateCount()))
+    print('')
 
-    # Enroll fingerprint
     try:
-        print('Place your finger on the sensor...')
-        while not f.readImage():
-            pass
+            print('*Waiting For Finger*')
+            print('')
 
-        f.convertImage(0x01)
-        result = f.searchTemplate()
-        position_number = result[0]
+            # Wait for finger to be read
 
-        if position_number == -1:
-            print('Template not found. Creating a new template...')
-            f.createTemplate()
-            position_number = f.storeTemplate()
-            template_filename = os.path.join(data_folder, 'template_{}.dat'.format(position_number))
-            
-            # Download the template characteristics
-            position_data = f.downloadCharacteristics(position_number)
+            while (f.readImage() == False):
+                pass
 
-            with open(template_filename, 'wb') as template_file:
-                template_file.write(position_data)
+            f.convertImage(0x01)
 
-            # Save the name along with the position number
-            name_filename = os.path.join(data_folder, 'name_{}.txt'.format(position_number))
-            with open(name_filename, 'w') as name_file:
-                name_file.write(name)
-            print('Fingerprint enrolled successfully. Position Number: {}'.format(position_number))
-        else:
-            print('Fingerprint already enrolled at position #{}'.format(position_number))
+            result = f.searchTemplate()
+            positionNumber = result[0]
+
+            if (positionNumber >= 0):
+                print('Fingerprint Already Exists at #' + str(positionNumber+1))
+                print('')
+                return None
+            else:
+                print('*Remove Finger*')
+                print('')
+                print('*Place Finger Again*')
+                print('')
+
+                # waiting for second read
+                while (f.readImage() == False):
+                    pass
+
+                f.convertImage(0X02)
+
+                if (f.compareCharacteristics() == 0):
+                    print('Fingers Do Not Match, Try again')
+                    print('')
+                    return None
+
+                f.createTemplate()
+
+                positionNumber = f.storeTemplate()
+
+                f.loadTemplate(positionNumber, 0X01)
+
+                characteristics = str(f.downloadCharacteristics(0x01)).encode('utf-8')
+                
+                print('Fingerprint Registered In Position #' + str(positionNumber+1))
+                print('')
+                data.write(str(positionNumber) + name + ' '+ '\n') 
+                data.flush()
+                data.close()
+    
     except Exception as e:
-        print('Error enrolling fingerprint: ' + str(e))
+        data.close()
+        print('Operation failed- Exception message: ' + str(e))
+        return None
+        
+def attendance():
+    t1=1
+    while t1!=0:
+        print('1. Take reading')
+        print('0. Quit')
+        print('Place Your finger...')
+        if t1==1:
+            while (f.readImage() == False):
+                pass
+            f.convertImage(0X01)  
+            result = f.searchTemplate()
+            if result[0]==-1:
+                print('No Match Found')
+                print('')
+                return None
+            else:
+                print('Found template at position #' + str(result[0]+1))
+                print('')    
+
+###################################################################################################################################################
+###################################################################################################################################################
 
 if __name__ == '__main__':
-    enroll_fingerprint()
+    t=1
+    while t!=0:
+        print("1. Enroll a new fingerprint")
+        print('2. Attendance')
+        print("3. Clear Database")
+        t=str(input('Select Funtion :'))
+        if t=='1':
+            enroll_fingerprint()
+        elif t=='2':
+            attendance()    
+        elif t=='3':
+            clear_database()  
+            
+        else:
+            t=0     
+
