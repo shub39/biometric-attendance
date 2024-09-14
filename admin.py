859df3b4e61630c7258b35d9165639593f551745
Shubham <cptnshubham39@gmail.com>
2024-09-14 09:14:53 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,3 +1,6 @@
+# Script to enroll data and train face dataset
+# Run this in GUI through VNC or HDMI
+
 import cv2
 import numpy as np
 import os
@@ -45,35 +48,37 @@ except Exception as e:
 def main_menu():
     global name, roll
     while True:
-        print("\n-> ADMIN SCRIPT")
+        print("\n---------------------------- ADMIN SCRIPT ---------------------------------")
         print("1. Capture Info")
         print("2. Train Image Dataset")
         print("3. Exit Program")
         print("4. Clear Database\n")
+        print("-----------------------------------------------------------------------------\n")
+
         
-        option = int(input("\nEnter Your Choice: "))
+        option = int(input("\n[INPUT] Enter Your Choice: "))
         if option == 1:
-            name = input("\nEnter Name: ")
-            roll = int(input("\nEnter Roll no: "))
+            name = input("[INPUT] Enter Name: ")
+            roll = int(input("[INPUT] Enter Roll no: "))
             capture_fingerprint()
             capture_face()
             write_data()
         elif option == 2:
             train_dataset()
         elif option == 3:
-            print("\nExiting the program. Goodbye!")
+            print("\n[BYE] Exiting the program. Goodbye!")
             break
         elif option ==4:
             clear_database()
         else:
-            print("\nInvalid choice. Please enter a valid option.\n")
+            print("\n[E] Invalid choice. Please enter a valid option.\n")
                   
 def capture_fingerprint():
     global roll, name, index
-    print('\nFINGERPRINTS STORED: ' + str(f.getTemplateCount()) + '\n')
+    print('\n[INFO] FINGERPRINTS CURRENTLY STORED: ' + str(f.getTemplateCount()) + '\n')
     
     try:
-        print('\n-> PLACE FINGER\n')
+        print('[ACTION] PLACE FINGER...\n')
         sleep(1)
         
         while not f.readImage():
@@ -84,18 +89,19 @@ def capture_fingerprint():
         positionNumber = result[0]
         
         if positionNumber >= 0:
-            print('\nAlready exists at #' + str(positionNumber + 1) + 'Try Again (y/n): ')
-            choice = input("Enter your Choice: ")
+            print('\n[INFO] Already exists at #' + str(positionNumber + 1) + 'Try Again (y/n): ')
+            choice = input("[INPUT] Enter your Choice: \n")
             if choice == "y":
                 capture_fingerprint()
             return None
         
         else:
-            print('REMOVE FINGER\n')
+            print('[ACTION] REMOVE FINGER\n')
+
             while f.readImage():
                 pass
             
-            print('PLACE FINGER AGAIN\n')
+            print('[ACTION] PLACE FINGER AGAIN\n')
             sleep(0.5)
             
             while not f.readImage():
@@ -104,7 +110,7 @@ def capture_fingerprint():
             f.convertImage(0x02)
             
             if f.compareCharacteristics() == 0:
-                print('ERROR...TRY AGAIN? (y/n)\n')
+                print('[E] FINGERPRINTS DONT MATCH. TRY AGAIN? (y/n)\n')
                 choice = input("Enter your Choice: ")
                 if choice == "y":
                     capture_fingerprint()
@@ -113,13 +119,11 @@ def capture_fingerprint():
             f.convertImage(0x02)
             f.createTemplate()
             positionNumber = f.storeTemplate()
-            
-            print('FINGERPRINT REGISTERED at ' + str(positionNumber + 1) + '\n')
-            
+            print('[INFO] FINGERPRINT REGISTERED AT #' + str(positionNumber + 1) + '\n')
             index = str(positionNumber + 1)
 
     except Exception as e:
-        print('Operation failed- Exception message: ' + str(e) + '\n')
+        print('[E] CAPTURE FINGERPRINT FAILED - ' + str(e) + '\n')
         return None
 
 def write_data():
@@ -132,14 +136,14 @@ def write_data():
     index = 0
     name = ""
     
-    print("-> DATA WRITTEN\n")
+    print("[INFO] DATA WRITTEN\n")
 
 def train_dataset():
-    print(f"\n[INFO] Training on dataset...\n")
+    print(f"\n[INFO] TRAINING FACE MODEL\n")
     faces, ids = getImagesAndLabels(path)
     trainRecognizer(faces, ids)
     faces_trained = len(set(ids))
-    print(f"\n[INFO] {faces_trained} faces trained.\n")
+    print(f"\n[INFO] {faces_trained} FACES TRAINED.\n")
     
 def getImagesAndLabels(path):
     faceSamples = []
@@ -159,19 +163,19 @@ def getImagesAndLabels(path):
 
     return faceSamples, ids
 
-def clear_database(): #Clears the database 
+def clear_database():
     f.clearDatabase()
     file1 = open('studentdata.csv','w')
     file1.close()
     try:
         shutil.rmtree("old_dataset")
     except OSError as e:
-        print("No Old Dataset folder")
+        print("[INFO] NO OLD DATASET FOLDER")
     try:
         shutil.rmtree("dataset")
     except OSError as e:
-        print("No Dataset folder")
-    print('All Data Cleared\n')
+        print("[INFO] NO DATASET FOLDER")
+    print('[INFO] ALL DATA CLEARED\n')
     sleep(2)
 
 def trainRecognizer(faces, ids):
@@ -190,6 +194,7 @@ def capture_face():
     cam.configure("preview")
     cam.start()
     count = 0
+
     while True:
         frame = cam.capture_array()
         cv2.putText(frame, 'Count:' + str(int(count)), POS, FONT, HEIGHT, TEXTCOLOR, WEIGHT)
@@ -217,12 +222,14 @@ def capture_face():
 
         cv2.imshow('FaceCapture', frame)
         key = cv2.waitKey(100) & 0xff
+
         if key == 27: 
             break
         elif key == 113:  
             break
         elif count >= COUNT_LIMIT: 
             break
+
     print("\n[INFO] Exiting Program and cleaning up stuff")
     cv2.destroyAllWindows()
     cam.close()
